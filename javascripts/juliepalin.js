41a951e5b4500bb044f60f1f916dc8c0db2d7a19
Dominic <dominic.martineau@ellipse-synergie.com>
2014-10-07 21:36:48 +0400

    Add social buttons

--- javascripts/juliepalin.js
+++ javascripts/juliepalin.js
@@ -1,6 +1,11 @@
 
 $(document).ready(function() {
 
+	$.stellar({
+        horizontalScrolling: false,
+        verticalOffset: 40
+    });
+
 	$('#header header a').on('click', function() {
         $('html, body').animate({
             scrollTop: ($($(this).attr('href')).offset().top - 60)
